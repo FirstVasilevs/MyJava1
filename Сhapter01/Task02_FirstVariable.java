6d6d31dbb5d99ea6bb2cf6e82c63a74c067add19
FirstVasilevs <81626051+FirstVasilevs@users.noreply.github.com>
2021-12-05 18:05:50 +0300

    Add files via upload

---
+++ Сhapter01/Task02_FirstVariable.java
@@ -0,0 +1,12 @@
+package ru.ereshchenko.MyJava.Сhapter01;
+
+public class Task02_FirstVariable {
+
+    public static void main(String[] args) {
+
+        String message = "Начальное значение";
+        System.out.println(message);
+        message = "Измененное значение";
+        System.out.println(message);
+    }
+}
---
+++ Сhapter01/Task05_Test.java
@@ -0,0 +1,9 @@
+package ru.ereshchenko.MyJava.Сhapter01;
+
+public class Task05_Test {
+
+    public static void main(String[] args) {
+
+
+    }
+}
