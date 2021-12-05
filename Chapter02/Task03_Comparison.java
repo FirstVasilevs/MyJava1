8302fcfecf022345c8128a9f3836533e1708c3b1
FirstVasilevs <81626051+FirstVasilevs@users.noreply.github.com>
2021-12-05 18:14:25 +0300

    Add files via upload

---
+++ Chapter02/Task03_Comparison.java
@@ -0,0 +1,24 @@
+package ru.ereshchenko.MyJava.Сhapter02;
+
+public class Task03_Comparison {
+
+    public static void main(String[] args) {
+
+        String txt = "Fantastic ";
+        String lang = "Java";
+
+        boolean state = (txt == lang);
+        System.out.println("Проверка строк на равенство: " + state);
+
+        state = (txt != lang);
+        System.out.println("Прверка на равенство " + state);
+
+        int dozen = 12;
+        int score = 20;
+        state = (dozen > score);
+        System.out.println("Проверка на больше: " + state);
+
+        state = (dozen < score);
+        System.out.println("Проверка на меньше: " + state);
+    }
+}
